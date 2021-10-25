02f60b2fc45e89d049b0175c15b10a2c227b0c5e
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-10-25 12:36:07 +0100

    Mon Oct 25 12:36:07 WAT 2021

--- entrypoint.sh
+++ entrypoint.sh
@@ -82,7 +82,7 @@ kubectl apply -n meshery -f ~/.meshery/ingress.yaml
 
 install_kubectl(){
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 }
 
 
